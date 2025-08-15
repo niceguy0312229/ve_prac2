2e9d85f922538c68713fffa09c835a26bb6aec8f
niceguy0312229 <jjjwhan@naver.com>
2025-08-15 16:03:59 -0400

    update

--- src/taxCal_2025_ex.py
+++ src/taxCal_2025_ex.py
@@ -1,4 +1,4 @@
-import numpy as np
+
 
 def calculate_cpp(income):
     YMPE = 71300
@@ -108,15 +108,3 @@ for salary in [100000, 178000, 200000]:
 
 
 
-matrix1 = np.array([[1, 2],
-                   [3, 4]])
-
-print(matrix1.shape)  # (2, 2) → (rows, columns)
-print(matrix1.size)   # 4     → total elements
-print(matrix1[0][1])  # Outputs 2 (row 1, column 2 in MATLAB)
-print(matrix1[1][0])  # Outputs 3 (row 2, column 1 in MATLAB)
-print(len(matrix1))
-
-matrix2 = np.array([[1, 2, 3 ], 
-                    [4, 5, 6 ]])
-print(matrix2.shape)
